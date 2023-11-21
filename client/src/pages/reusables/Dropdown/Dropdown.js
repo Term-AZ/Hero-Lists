a57a68308e9a8c9dd23500cd411760e02ed8491b
AugustZilakovs <106542089+Term-AZ@users.noreply.github.com>
2023-11-20 21:56:34 -0500

    attempt at cors

---
+++ client/src/pages/reusables/Dropdown/Dropdown.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Dropdown.css'
+
+const Dropdown = () =>{
+    return(
+       <div>
+        
+       </div> 
+    )
+
+}
+
+export default Dropdown
--- server/index.js
+++ server/index.js
@@ -4,10 +4,23 @@ var app = express();
 const db = require('../db/db.js')
 const dotenv = require('dotenv')
 const jwt = require('jsonwebtoken')
+const bodyParser = require('body-parser')
 
 app.use(cors({
     credentials: true, origin: 'http://localhost:3000'
 }))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// var corsOptions = {
+//     origin: function (origin, callback) {
+//       // db.loadOrigins is an example call to load
+//       // a list of origins from a backing database
+//       db.loadOrigins(function (error, origins) {
+//         callback(error, origins)
+//       })
+//     }
+//   }
 
 dotenv.config()
 const port = process.env.port;
@@ -20,9 +33,10 @@ app.post("/user/generateToken", (req,res)=>{
     let jwtSecretKey = process.env.JWT_SECRET_KEY  
 })
 
-app.get("/superheroLists/heros/data", (req,res)=>{
+app.get("/superheroLists/heros/data", function(req,res){
     var q = "SELECT hero_name FROM superheros"
     db.query(q, (err,result, fields)=>{
+        res.setHeader('Access-Control-Allow-Origin',true)
         res.send(result)
     })
 })
